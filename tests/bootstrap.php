09cd9070d61c9037ad86b48de43cd9b8782c3727
romeOz <serggalka@gmail.com>
2015-10-12 16:52:51 +0300

    Added alias to path.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,6 @@
 <?php
+use rock\base\Alias;
+
 $composerAutoload = dirname(__DIR__) . '/vendor/autoload.php';
 if (is_file($composerAutoload)) {
     /** @var \Composer\Autoload\ClassLoader $loader */
@@ -11,4 +13,5 @@ require(dirname(__DIR__) . '/src/polyfills.php');
 
 date_default_timezone_set('UTC');
 
+Alias::setAlias('rockunit', __DIR__);
 define('ROCKUNIT_RUNTIME', __DIR__ . '/runtime');
